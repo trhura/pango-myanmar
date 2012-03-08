ed081c84d4257c041ae10c7b286b7261430ddefd
Thura Hlaing <trhura@gmail.com>
2012-03-08 10:31:35 +0630

    Initial commit

---
+++ src/myanmar-lang.c
@@ -0,0 +1,145 @@
+/* Pango
+ * myanmar-lang.c: Language Engine for Myanmar script
+ *
+ * Copyright (C) 2005 Tin Myo Htet <tmhtet@gmail.com>
+ *
+ * Based on Language Engine from pango-libthai
+ * Copyright (C) 2003 Theppitak Karoonboonyanan <thep@linux.myanmar.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+#include <glib.h>
+#include <pango/pango-engine.h>
+#include <pango/pango-break.h>
+
+#include "myanmar.h"
+
+/* No extra fields needed */
+typedef PangoEngineLang		 MyanmarEngineLang;
+typedef PangoEngineLangClass MyanmarEngineLangClass;
+
+#define SCRIPT_ENGINE_NAME "MyanmarScriptEngineLang"
+
+static PangoEngineScriptInfo myanmar_scripts[] = {
+{ PANGO_SCRIPT_MYANMAR, "*" }
+};
+
+
+static PangoEngineInfo script_engines[] = {
+	{
+		SCRIPT_ENGINE_NAME,
+		PANGO_ENGINE_TYPE_LANG,
+		PANGO_RENDER_TYPE_NONE,
+		myanmar_scripts, G_N_ELEMENTS(myanmar_scripts)
+	}
+};
+
+
+static void
+myanmar_engine_break (PangoEngineLang *engine,
+					  const char	  *text,
+					  int			   length,
+					  PangoAnalysis	  *analysis,
+					  PangoLogAttr	  *attrs,
+					  int			   attrs_len)
+{
+
+	gint i;
+	glong n_chars;
+	gunichar *wcs = g_utf8_to_ucs4_fast (text, length, &n_chars);
+
+	for (i = 0; i < n_chars; i++)
+	{
+		GUnicodeType type = g_unichar_type (wcs[i]);
+
+		if (type == G_UNICODE_OTHER_LETTER) {
+			/* All letters (consonants) are character,line breaks.
+			 */
+			attrs[i].is_char_break = TRUE;
+			attrs[i].is_line_break = TRUE;
+			attrs[i].is_cursor_position = TRUE;
+
+			/* Skip marks ...*/
+			gint next = 1;
+			while ((i+next) < n_chars &&
+				   g_unichar_type (wcs[i+next]) == G_UNICODE_NON_SPACING_MARK)
+			{
+				if (wcs[i+next] == MYANMAR_SIGN_ASAT)
+				{
+					/* If it is followed ASAT, don't break . */
+					attrs[i].is_line_break = FALSE;
+					attrs[i].is_char_break = FALSE;
+					attrs[i].is_cursor_position = FALSE;
+
+					/* Howver, if it is infront of kinzi (followed by virama),
+					 * cursor position break should be allowed .
+					 */
+					if ((i+next+1) < n_chars &&
+						wcs[i+next+1] == MYANMAR_SIGN_VIRAMA)
+						attrs[i].is_cursor_position = TRUE;
+					break;
+				}
+				next++;
+			}
+
+			/* For stacked consonants & (after) kinzi, doesn't allow break */
+			if (i > 0 && wcs[i-1] == MYANMAR_SIGN_VIRAMA) {
+				attrs[i].is_line_break = FALSE;
+				attrs[i].is_char_break = FALSE;
+				attrs[i].is_cursor_position = FALSE;
+			}
+		}
+	}
+}
+
+static void
+myanmar_engine_lang_class_init (PangoEngineLangClass *class)
+{
+	class->script_break = myanmar_engine_break;
+}
+
+PANGO_ENGINE_LANG_DEFINE_TYPE (MyanmarEngineLang, myanmar_engine_lang,
+							   myanmar_engine_lang_class_init, NULL);
+
+void
+PANGO_MODULE_ENTRY(init) (GTypeModule *module)
+{
+	myanmar_engine_lang_register_type (module);
+}
+
+void
+PANGO_MODULE_ENTRY(exit) (void)
+{
+}
+
+void
+PANGO_MODULE_ENTRY(list) (PangoEngineInfo **engines, gint *n_engines)
+{
+	*engines = script_engines;
+	*n_engines = G_N_ELEMENTS (script_engines);
+}
+
+/* Load a particular engine given the ID for the engine */
+PangoEngine *
+PANGO_MODULE_ENTRY(create) (const char *id)
+{
+	if (!strcmp (id, SCRIPT_ENGINE_NAME))
+		return g_object_new (myanmar_engine_lang_type, NULL);
+	else
+		return NULL;
+}
---
+++ src/myanmar.h
@@ -0,0 +1,124 @@
+/* pango-myanmar
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Thura Hlaing <trhura@gmail.com>
+ *
+ */
+
+#ifndef __MYANMAR_H__
+#define __MYANMAR_H__
+
+#define		MYANMAR_LETTER_KA					0x1000
+#define		MYANMAR_LETTER_KHA					0x1001
+#define		MYANMAR_LETTER_GA					0x1002
+#define		MYANMAR_LETTER_GHA					0x1003
+#define		MYANMAR_LETTER_NGA					0x1004
+#define		MYANMAR_LETTER_CA					0x1005
+#define		MYANMAR_LETTER_CHA					0x1006
+#define		MYANMAR_LETTER_JA					0x1007
+#define		MYANMAR_LETTER_JHA					0x1008
+#define		MYANMAR_LETTER_NYA					0x1009
+#define		MYANMAR_LETTER_NNYA					0x100a
+#define		MYANMAR_LETTER_TTA					0x100b
+#define		MYANMAR_LETTER_TTHA					0x100c
+#define		MYANMAR_LETTER_DDA					0x100d
+#define		MYANMAR_LETTER_DDHA					0x100e
+#define		MYANMAR_LETTER_NNA					0x100f
+#define		MYANMAR_LETTER_TA					0x1010
+#define		MYANMAR_LETTER_THA					0x1011
+#define		MYANMAR_LETTER_DA					0x1012
+#define		MYANMAR_LETTER_DHA					0x1013
+#define		MYANMAR_LETTER_NA					0x1014
+#define		MYANMAR_LETTER_PA					0x1015
+#define		MYANMAR_LETTER_PHA					0x1016
+#define		MYANMAR_LETTER_BA					0x1017
+#define		MYANMAR_LETTER_BHA					0x1018
+#define		MYANMAR_LETTER_MA					0x1019
+#define		MYANMAR_LETTER_YA					0x101a
+#define		MYANMAR_LETTER_RA					0x101b
+#define		MYANMAR_LETTER_LA					0x101c
+#define		MYANMAR_LETTER_WA					0x101d
+#define		MYANMAR_LETTER_SA					0x101e
+#define		MYANMAR_LETTER_HA					0x101f
+#define		MYANMAR_LETTER_LLA					0x1020
+#define		MYANMAR_LETTER_A					0x1021
+#define		MYANMAR_LETTER_SHAN_A				0x1022
+#define		MYANMAR_LETTER_I					0x1023
+#define		MYANMAR_LETTER_II					0x1024
+#define		MYANMAR_LETTER_U					0x1025
+#define		MYANMAR_LETTER_UU					0x1026
+#define		MYANMAR_LETTER_E					0x1027
+#define		MYANMAR_LETTER_MON_E				0x1028
+#define		MYANMAR_LETTER_O					0x1029
+#define		MYANMAR_LETTER_AU					0x102a
+#define		MYANMAR_VOWEL_SIGN_TALL_AA			0x102b
+#define		MYANMAR_VOWEL_SIGN_AA				0x102c
+#define		MYANMAR_VOWEL_SIGN_I				0x102d
+#define		MYANMAR_VOWEL_SIGN_II				0x102e
+#define		MYANMAR_VOWEL_SIGN_U				0x102f
+#define		MYANMAR_VOWEL_SIGN_UU				0x1030
+#define		MYANMAR_VOWEL_SIGN_E				0x1031
+#define		MYANMAR_VOWEL_SIGN_AI				0x1032
+#define		MYANMAR_VOWEL_SIGN_MON_II			0x1033
+#define		MYANMAR_VOWEL_SIGN_MON_O			0x1034
+#define		MYANMAR_VOWEL_SIGN_E_ABOVE			0x1035
+#define		MYANMAR_SIGN_ANUSVARA				0x1036
+#define		MYANMAR_SIGN_DOT_BELOW				0x1037
+#define		MYANMAR_SIGN_VISARGA				0x1038
+#define		MYANMAR_SIGN_VIRAMA					0x1039
+#define		MYANMAR_SIGN_ASAT					0x103a
+#define		MYANMAR_CONSONANT_SIGN_MEDIAL_YA	0x103b
+#define		MYANMAR_CONSONANT_SIGN_MEDIAL_RA	0x103c
+#define		MYANMAR_CONSONANT_SIGN_MEDIAL_WA	0x103d
+#define		MYANMAR_CONSONANT_SIGN_MEDIAL_HA	0x103e
+#define		MYANMAR_LETTER_GREAT_SA				0x103f
+#define		MYANMAR_DIGIT_ZERO					0x1040
+#define		MYANMAR_DIGIT_ONE					0x1041
+#define		MYANMAR_DIGIT_TWO					0x1042
+#define		MYANMAR_DIGIT_THREE					0x1043
+#define		MYANMAR_DIGIT_FOUR					0x1044
+#define		MYANMAR_DIGIT_FIVE					0x1045
+#define		MYANMAR_DIGIT_SIX					0x1046
+#define		MYANMAR_DIGIT_SEVEN					0x1047
+#define		MYANMAR_DIGIT_EIGHT					0x1048
+#define		MYANMAR_DIGIT_NINE					0x1049
+#define		MYANMAR_SIGN_LITTLE_SECTION			0x104a
+#define		MYANMAR_SIGN_SECTION				0x104b
+#define		MYANMAR_SYMBOL_LOCATIVE				0x104c
+#define		MYANMAR_SYMBOL_COMPLETED			0x104d
+#define		MYANMAR_SYMBOL_AFOREMENTIONED		0x104e
+#define		MYANMAR_SYMBOL_GENITIVE				0x104f
+
+#define		LEFT_PARENTHESIS					0x0028
+#define		RIGHT_PARENTHESIS					0x0029
+#define		PERCENT_SIGN						0x0025
+#define		SOLIDUS								0x002F
+#define		SUMMATION							0x2211
+#define		PLUS_SIGN							0x002B
+#define		MINUS_SIGN							0x2212
+#define		EQUALS_SIGN							0x003D
+#define		COLON								0x003A
+#define		FULL_STOP							0x002E
+#define		SPACE								0x0020
+#define		TAB									0x0009
+#define		LEFT_SINGLE_QUOTE					0x2018
+#define		LEFT_DOUBLE_QUOTE					0x201C
+#define		RIGHT_SINGLE_QUOTE					0x2019
+#define		RIGHT_DOUBLE_QUOTE					0x201D
+#define		ZERO_WIDTH_NON_JOINER				0x200C
+
+#endif /* __MYANMAR_H__ */
